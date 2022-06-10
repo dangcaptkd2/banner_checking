ade6b0aa4ff37969586d712259781eec1a632255
dangcaptkd2 <thinhquyen9461@gmail.com>
2022-06-10 15:28:48 +0700

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from mid_process import mid_process, action_merge
-from policy_checking import check_text_eng, check_text_vi
+from utils.mid_process import mid_process, action_merge
+from utils.policy_checking import check_text_eng, check_text_vi
 from utils.utils import call_api_vi, call_api_nsfw, check_is_vn, clear_folder
