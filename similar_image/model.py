cb533f191fbe26e6613d11681b467848557222b7
dangcaptkd2 <thinhquyen9461@gmail.com>
2023-05-08 11:45:11 +0700

    update

--- similar_image/model.py
+++ similar_image/model.py
@@ -6,7 +6,7 @@ import torchvision.models as models
 class Network(nn.Module):
     def __init__(self):
         super(Network, self).__init__()
-        resnet50 = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
+        resnet50 = models.resnet50(weights=None)
         self.model = nn.Sequential(*list(resnet50.children())[:-1])
         
     def forward(self, x):
