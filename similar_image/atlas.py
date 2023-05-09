6b0aca1714fd4550b7df2bb32f381d85c21f31a6
dangcaptkd2 <thinhquyen9461@gmail.com>
2023-05-09 14:56:51 +0700

    update

--- similar_image/atlas.py
+++ similar_image/atlas.py
@@ -34,7 +34,7 @@ class SIMILAR_MODEL:
         features = features.squeeze().detach().numpy()
         return features
     
-    def check_similar(self, thres=0.999, img_path=None):
+    def check_similar(self, thres=0.9999, img_path=None):
         v = self._get_embedding(img_path=img_path)
         cos_sim_list = cosine_similarity([v], self.data)
         score = max(cos_sim_list[0])
